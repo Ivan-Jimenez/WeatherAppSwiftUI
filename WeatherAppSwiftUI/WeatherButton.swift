c56f3d51f1d1f8e43119fcea3d949f7076a0bbc4
Alexis Jimenex <ijimenez@pagando.mx>
2021-01-29 20:47:14 -0700

    First commit

--- WeatherAppSwiftUI/WeatherButton.swift
+++ WeatherAppSwiftUI/WeatherButton.swift
@@ -5,4 +5,20 @@
 //  Created by Alexis Jimenez on 29/01/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    var foregroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(foregroundColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
